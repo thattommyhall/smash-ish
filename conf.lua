992d43f7535fc3ca8ebb1668908d0550d6b65e5d
Tom Hall <thattommyhall@gmail.com>
2024-06-06 19:37:18 +0100

    make it bigger

--- conf.lua
+++ conf.lua
@@ -3,8 +3,8 @@ love.conf = function(t)
    t.title, t.identity = "smash-ish", "smash-ish"
    t.modules.joystick = false
    t.modules.physics = false
-   t.window.width = 720
-   t.window.height = 450
+   t.window.width = 800
+   t.window.height = 600
    t.window.vsync = false
    t.version = "11.5"
 end
